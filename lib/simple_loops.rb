76caaf9f79403871adfc90747a3276e36e4da4a5
Alex Ngundji <alexngundji@gmail.com>
2020-06-21 23:55:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,16 @@
 # Write your methods here
+def loop_message_five_times(number)
+#number = 5
+ 
+#number.times do
+  #puts "#{number} times"
+  5.times{puts number}
+end
+
+def loop_message_n_times(message, limit)
+  count = 0
+  while count < limit do
+    puts message
+    count += 1
+  end
+end
